446e31f2046232d100612e6c665212ca9e4a16ec
rivwox <rivera_augusto@protonmail.com>
2020-07-02 13:13:43 -0500

    some changes if added

--- pull.sh
+++ pull.sh
@@ -2,14 +2,22 @@
 
 dir=$(pwd)
 
-for i in $(find . -name ".git" -type d | cut -c 3- ); do
-	echo -e $i;
-	cd $i;
-	cd ..;
-	git pull;
+if [ $dir = $HOME/git ]; then
+	for i in $(find . -name ".git" -type d | cut -c 3- ); do
+		echo -e $i;
+		cd $i;
+		cd ..;
+		git pull;
+		echo "................"
+		cd $dir
+		
+	done
 
-	cd $dir
-done
+	echo ".........."
+	echo ""
+	echo "YAY!"
+else
+	echo "Debes estar en git" 
+fi
 
-echo "YAY!"
 
